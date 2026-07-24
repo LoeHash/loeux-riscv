bfaab9149d662bdbba0445ff18e0dee774540b18
LoeHash <2679792783@qq.com>
2026-07-24 15:27:47 +0800

    <init> init....

--- kernel/start.c
+++ kernel/start.c
@@ -6,6 +6,7 @@
 // dtb是遵循了这个fdt的规范, 所以对应的, ft_addr就是我们想要的扁平设备树
 void kstart(unsigned long hart_id, unsigned long ft_addr)
 {
+
         printk("hello world! this is: %lu\n", hart_id);
         while (1)
                 ;
