6f3c41ff67ad3f1d40de4569c54de495d067e56b
LoeHash <2679792783@qq.com>
2026-09-12 17:52:19 +0800

    <dev> slab大小对其函数: slab_round_up

--- mm/slab.c
+++ mm/slab.c
@@ -1,1 +1,24 @@
 #include <slab.h>
+#include <panic.h>
+
+/// @brief slab 对齐大小
+/// @param size 大小
+/// @return 对齐后的大小
+static inline uint32_t slab_round_up(uint32_t size)
+{
+        if (size == 0 || size > SLAB_SIZE(4096))
+                return 0;
+
+        if (size <= SLAB_SIZE(8))
+                return SLAB_SIZE(8);
+
+        size--;
+        size |= size >> 1;
+        size |= size >> 2;
+        size |= size >> 4;
+        size |= size >> 8;
+        size |= size >> 16;
+        size++;
+
+        return size;
+}
