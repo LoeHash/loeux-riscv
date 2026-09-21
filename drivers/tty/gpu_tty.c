55cb3f829dfcddfb6f9d385e778e6d3287534b5c
loehash <2679792783@qq.com>
2026-09-21 19:50:10 +0800

    <fix> 修复一些小bug，此时暂时先使用uart_get 作为输入

--- drivers/tty/gpu_tty.c
+++ drivers/tty/gpu_tty.c
@@ -1,15 +1,48 @@
 #include <tty.h>
 #include <virtio_gpu.h>
+#include <uart.h>
 #include <lib.h>
 #include <printk.h>
 #include <spinlock.h>
 #include <tty/gpu_tty.h>
 #include <kgfx.h>
 #include <ascii8x16.h>
+/**
+        gpu_tty 终端设备驱动
+                实现了 gpu tty 的基本功能，包括字符绘制、光标移动、换行等。
+                注意: 这玩意是一套驱动的集合体，如终端输出是用 kgfx->virtio_gpu 驱动
+                     而读取输入，会通过键盘驱动来实现
+                     同时这个终端设备也需要视觉反馈，并非像以前的uart tty 那样直接输出到宿主机终端
+                     这也为未来脱离宿主机，真正的运行在物理机上提供了可能！
+**/
 
 static void gpu_tty_advance_line(struct gpu_tty_state *st);
 static void gpu_tty_test(void);
+static void gpu_tty_erase_cursor(struct gpu_tty_state *st);
+static void gpu_tty_draw_cursor(struct gpu_tty_state *st);
 
+static void gpu_tty_draw_cursor(struct gpu_tty_state *st)
+{
+        // printk("draw_cursor: drawn=%d cur=(%u,%u)\n", st->cursor_drawn, st->cur_x, st->cur_y);
+	if (st->cursor_drawn)
+		return;
+
+	kgfx_fill_rect(st->gpu, st->cur_x, st->cur_y + 14,
+	               ASCII8X16_W, 2, 0xFFFFFFFF);
+	kgfx_update(st->gpu);
+	st->cursor_drawn = 1;
+}
+
+static void gpu_tty_erase_cursor(struct gpu_tty_state *st)
+{
+	if (!st->cursor_drawn)
+		return;
+
+	kgfx_fill_rect(st->gpu, st->cur_x, st->cur_y + 14,
+	               ASCII8X16_W, 2, st->bg);
+	kgfx_update(st->gpu);
+	st->cursor_drawn = 0;
+}
 
 static void gpu_tty_advance_line(struct gpu_tty_state *st)
 {
@@ -25,39 +58,40 @@ static void gpu_tty_advance_line(struct gpu_tty_state *st)
 
 static int gpu_tty_putc(struct tty *tty, char c)
 {
-        struct gpu_tty_state *st = tty->priv;
-        struct virtio_gpu_device *gpu = st->gpu;
+	struct gpu_tty_state *st = tty->priv;
+	struct virtio_gpu_device *gpu = st->gpu;
 
-        if (c == '\r') {
-                st->cur_x = 0;
-                return 0;
-        }
+	gpu_tty_erase_cursor(st);
 
-        if (c == '\n') {
-                gpu_tty_advance_line(st);
-                kgfx_update(gpu);
-                return 0;
-        }
+	if (c == '\r') {
+		st->cur_x = 0;
+	} else if (c == '\n') {
+		gpu_tty_advance_line(st);
+		kgfx_update(gpu);
+	} else if (c == '\b') {
+		if (st->cur_x >= ASCII8X16_W)
+			st->cur_x -= ASCII8X16_W;
+	} else {
+		if (st->cur_x + ASCII8X16_W > gpu->width)
+			gpu_tty_advance_line(st);
 
-        if (c == '\b') {
-                if (st->cur_x >= ASCII8X16_W)
-                st->cur_x -= ASCII8X16_W;
-                return 0;
-        }
+		kgfx_draw_char(gpu, c, st->cur_x, st->cur_y, st->fg, st->bg);
+		st->cur_x += ASCII8X16_W;
+		kgfx_update(gpu);
+	}
 
-        if (st->cur_x + ASCII8X16_W > gpu->width)
-                gpu_tty_advance_line(st);
+	if (st->cursor_visible)
+		gpu_tty_draw_cursor(st);
 
-        kgfx_draw_char(gpu, c, st->cur_x, st->cur_y, st->fg, st->bg);
-        st->cur_x += ASCII8X16_W;
-
-        kgfx_update(gpu);
-        return 0;
+	return 0;
 }
 
 static int gpu_tty_getc(struct tty *tty, char *out)
 {
+        // while(1);
+        *out = uart_getchar();
         return 0;
+        // return 0;
 }
 
 static int gpu_tty_has_input(struct tty *tty)
@@ -94,6 +128,9 @@ void init_gpu_tty()
         gpu0_tty_state.cur_y = 0;
         gpu0_tty_state.fg    = 0xFFFFFFFF;
         gpu0_tty_state.bg    = 0xFF000000; 
+        gpu0_tty_state.cursor_visible = 1;
+        gpu0_tty_state.cursor_drawn = 0;
+        gpu0_tty_state.blink_counter = 0;
 
         kgfx_clear(gpu0_tty_state.gpu, gpu0_tty_state.bg);
         kgfx_update(gpu0_tty_state.gpu);
